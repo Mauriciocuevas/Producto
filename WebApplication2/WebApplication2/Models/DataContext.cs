1b1b9e0eb812782d7abb18d501bcdc36257e367e
mauricio cuevas <reymauricoo@gmail.com>
2019-03-16 11:19:21 -0400

    primero y segundo commit

---
+++ WebApplication2/WebApplication2/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace WebApplication2.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<WebApplication2.Models.Producto> Productoes { get; set; }
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/Producto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Producto
+    {
+        [Key]
+        public int ProductoID { get; set; }
+        [Required]//se ha requerido el double
+        [Display(Name ="Nombre del Producto")]//con lo identificamos el producto
+
+        public string Nombre { get; set; }
+
+        public double Precio { get; set; }
+    }
+}
